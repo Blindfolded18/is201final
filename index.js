f70ed2d8ef856ee4807027efd8e4b44c22ad0feb
AyakaMain <phaomaxe2015@gmail.com>
2023-11-19 15:20:09 -0800

    done index page

--- index.js
+++ index.js
@@ -1,43 +1,75 @@
-const diaryEntries = [];
 const SELECTED_CLASS_NAME = "selected";
 const editMoodsElem = document.querySelector("#edit-moods");
 const diaryListElem = document.querySelector("#diary-list");
 const addEntryElem = document.querySelector("#add-entry");
+const diaryEditElem = document.querySelector("#diary-edit");
 const editDateInnerElem = document.querySelector("#edit-date input");
 const editNoteInnerElem = document.querySelector("#edit-note textarea");
-const saveEntryElem = document.querySelector("#save-entry");
-const moodOptionElems = moods.map((mood, i) => {
+moods.forEach((mood, i, moods) => {
     const moodOptionElem = document.createElement("button");
     moodOptionElem.classList.add("mood-option");
-    moodOptionElem.value = `${i}`;
+    // moodOptionElem.value = `${i}`;
 
     const moodCircle = document.createElement("span");
     moodCircle.classList.add("mood-color", mood.clazz);
     moodOptionElem.append(moodCircle, mood.name);
-    moodOptionElem.addEventListener("click", () => {
-        moodOptionElem.classList.toggle(SELECTED_CLASS_NAME);
-    });
 
     editMoodsElem.appendChild(moodOptionElem);
-    return moodOptionElem;
+    moods[i].elem = moodOptionElem;
 });
 
 class DiaryEntry {
     date = new Date();
-    moods = new Array(moods.length).fill(false)
+    moods = new Set();
     note = "";
 
     loadTo(editDateInnerElem, moodOptionElems, editNoteInnerElem) {
-        editDateInnerElem.value = this.date;
-        this.moods.forEach((hasMood, i) => {
-            if (hasMood) {
-                moodOptionElems[i].classList.add(SELECTED_CLASS_NAME);
-            } else {
-                moodOptionElems[i].classList.remove(SELECTED_CLASS_NAME);
-            }
+        editDateInnerElem.value = this.date.toISOString().split('T')[0];
+        moodOptionElems.forEach(moodOptionElem => moodOptionElem.classList.remove(SELECTED_CLASS_NAME));
+        this.moods.forEach(mood => {
+            mood.elem.classList.add(SELECTED_CLASS_NAME);
         });
         editNoteInnerElem.value = this.note;
     }
+}
+
+class EntryManager {
+    diaryEntries = [];
+    currentEntry = null;
+
+    removeEntry(entry) {
+        // Maybe code related to database
+
+        const removeI = this.diaryEntries.findIndex(entryInner => entryInner === entry);
+        if (removeI < 0) {
+            return false;
+        }
+
+        this.diaryEntries.splice(removeI, 1);
+
+        if (this.currentEntry === entry) {
+            this.currentEntry = null;
+            return true;
+        }
+        return false;
+    }
+}
+
+function dateToVerbose(date) {
+    return date.toLocaleDateString(
+        undefined,
+        {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        },
+    );
+}
+
+const entryManager = new EntryManager();
+const editObserver = {
+    entryDateElem: null,
+    moodsElem: null,
 }
 
 // Add entry event
@@ -50,30 +82,33 @@ addEntryElem.addEventListener("click", () => {
     {
         const entryInfoElem = document.createElement("div");
         entryInfoElem.classList.add("entry-info");
-        {
-            const entryDateElem = document.createElement("p");
-            entryDateElem.classList.add("entry-date");
-            entryDateElem.textContent = diaryEntry.date.toLocaleDateString(
-                undefined,
-                {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric'
-                },
-            );
-            entryInfoElem.appendChild(entryDateElem);
-        }
-        {
-            const moodsElem = document.createElement("div");
-            moodsElem.classList.add("moods");
-            entryInfoElem.appendChild(moodsElem);
-        }
-        entryInfoElem.addEventListener("click", () => {
+
+        const entryDateElem = document.createElement("p");
+        entryDateElem.classList.add("entry-date");
+        entryDateElem.textContent = dateToVerbose(diaryEntry.date);
+        entryInfoElem.appendChild(entryDateElem);
+
+        const moodsElem = document.createElement("div");
+        moodsElem.classList.add("moods");
+        entryInfoElem.appendChild(moodsElem);
+
+        const loader = () => {
             document.querySelectorAll(".entry-info.selected")
                 .forEach(entryInfoElem => entryInfoElem.classList.remove(SELECTED_CLASS_NAME));
             entryInfoElem.classList.add(SELECTED_CLASS_NAME);
-            diaryEntry.loadTo(editDateInnerElem, moodOptionElems, editNoteInnerElem);
-        });
+            entryManager.currentEntry = diaryEntry;
+            diaryEntry.loadTo(
+                editDateInnerElem,
+                moods.map(mood => mood.elem),
+                editNoteInnerElem
+            );
+            diaryEditElem.style.visibility = "visible";
+
+            editObserver.entryDateElem = entryDateElem;
+            editObserver.moodsElem = moodsElem;
+        };
+        entryInfoElem.addEventListener("click", loader);
+        loader(); // load the new created entry right away
         diaryEntryElem.appendChild(entryInfoElem);
     }
     {
@@ -83,13 +118,64 @@ addEntryElem.addEventListener("click", () => {
         deleteEntryElem.addEventListener("click", () => {
             if (confirm("Are you sure to delete this entry?")) {
                 diaryEntryElem.remove();
-                const removeI = diaryEntries.findIndex(entry => entry === diaryEntry);
-                diaryEntries.splice(removeI, 1);
+                if (entryManager.removeEntry(diaryEntry)) { // if we delete the currently displayed entry
+                    diaryEditElem.style.visibility = "hidden";
+
+                    // For security purpose, nullify all recorded info
+                    editDateInnerElem.value = "";
+                    moods
+                        .map(mood => mood.elem)
+                        .forEach(
+                            moodOptionElem => moodOptionElem.classList.remove(SELECTED_CLASS_NAME)
+                        );
+                    editNoteInnerElem.value = "";
+                }
             }
         });
         diaryEntryElem.appendChild(deleteEntryElem);
     }
     diaryListElem.insertBefore(diaryEntryElem, addEntryElem);
 
-    diaryEntries.push(diaryEntry);
+    entryManager.diaryEntries.push(diaryEntry);
+});
+
+editDateInnerElem.addEventListener("change", () => {
+    // We can't just use Date's constructor since the timezone is different here
+    const [year, month, day] = editDateInnerElem.value.split('-').map(Number);
+    const date = new Date(year, month - 1, day);
+
+    entryManager.currentEntry.date = date;
+    editObserver.entryDateElem.textContent = dateToVerbose(date);
+});
+
+moods
+    .forEach((mood, _, moods) => {
+        mood.elem.addEventListener("click", () => {
+            if (mood.elem.classList.toggle(SELECTED_CLASS_NAME)) {
+                entryManager.currentEntry.moods.add(mood);
+            } else {
+                entryManager.currentEntry.moods.delete(mood);
+            }
+
+            editObserver.moodsElem.innerHTML = "";
+            moods
+                .filter(mood => mood.elem.classList.contains(SELECTED_CLASS_NAME))
+                .forEach(mood => {
+                    const moodElem = document.createElement("span");
+                    moodElem.classList.add("mood");
+
+                    {
+                        const moodColor = document.createElement("span");
+                        moodColor.classList.add("mood-color", mood.clazz);
+                        moodElem.appendChild(moodColor);
+                    }
+                    moodElem.append(mood.name);
+
+                    editObserver.moodsElem.appendChild(moodElem);
+                })
+        });
+    });
+
+editNoteInnerElem.addEventListener("change", () => {
+    entryManager.currentEntry.note = editNoteInnerElem.value;
 });
