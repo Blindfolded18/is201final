af7333ba29ab08b68f905e0433fa3499098c6ccf
AyakaMain <phaomaxe2015@gmail.com>
2023-11-29 17:55:14 -0800

    dark theme completed, except the background image

--- global.js
+++ global.js
@@ -1,5 +1,25 @@
-const settingsButton = document.getElementById('setting');
+const DARK_THEME_KEY = "dark-theme";
+const settingsButton = document.querySelector('#setting');
+
+function updateTheme() {
+    document.querySelectorAll("*")
+        .forEach(elem => {
+            if (sessionStorage.getItem(DARK_THEME_KEY) === null) {
+                elem.classList.remove("dark-theme");
+            } else {
+                elem.classList.add("dark-theme");
+            }
+        });
+}
 
 settingsButton.addEventListener('click', () => {
-    document.querySelectorAll("*").forEach(elem => elem.classList.toggle("dark-theme"))
+    if (sessionStorage.getItem(DARK_THEME_KEY) === null) {
+        sessionStorage.setItem(DARK_THEME_KEY, "");
+    } else {
+        sessionStorage.removeItem(DARK_THEME_KEY);
+    }
+
+    updateTheme();
 });
+
+updateTheme();
